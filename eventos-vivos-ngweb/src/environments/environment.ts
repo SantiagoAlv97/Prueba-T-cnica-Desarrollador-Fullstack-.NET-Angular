298db846b09505127f9a67f450dabdc3351010e1
SAlvarez <santiagoalvarezcarvajal@gmail.com>
2026-06-18 11:21:51 -0500

    Agregar url de la API de azure

--- eventos-vivos-ngweb/src/environments/environment.ts
+++ eventos-vivos-ngweb/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: '/api',
+    apiUrl: 'https://eventovivosapi-dwecejgtfwb5fkd8.centralus-01.azurewebsites.net',
     googleClientId: '334068155943-t3ik7ui10b4j2p6nikqvoub72i0c9t5k.apps.googleusercontent.com'
 };
